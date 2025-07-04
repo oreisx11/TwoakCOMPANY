c257796b27f1b85fb3a19a525089c41447a8d2d2
oreisx11 <luanfbrz2702@gmail.com>
2025-07-04 17:10:39 -0300

    TW V.1 final

--- contato.js
+++ contato.js
@@ -8,6 +8,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const telefone = document.getElementById('telefone').value;
     const empresa = document.getElementById('empresa').value;
     const cargo = document.getElementById('cargo').value;
+    const assunto = document.getElementById('assunto').value;
 
     // Formata a mensagem
     const mensagem = `Novo contato da Twoak Company:%0A` +
@@ -15,7 +16,8 @@ document.getElementById('contactForm').addEventListener('submit', function(event
                      `Email: ${email}%0A` +
                      `Telefone: ${telefone}%0A` +
                      `Empresa: ${empresa}%0A` +
-                     `Cargo: ${cargo}`;
+                     `Cargo: ${cargo}%0A` +
+                     `Assunto: ${assunto}`;
 
     // Número do WhatsApp (sem espaços, + ou parênteses)
     const numeroWhatsApp = '+5511970629125';
